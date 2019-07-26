fdfe8164b9f54a3976ff4bf745138cde5d55c832
Carlos Vargas <carvarjr_95@hotmail.com>
2019-07-26 04:41:33 -0500

    POO

---
+++ Portatil/src/ejemplotutoria/Portatil.java
@@ -0,0 +1,30 @@
+package ejemplotutoria;
+
+/**
+ *
+ * @author cavargas10
+ */
+public class Portatil {
+
+    String color;
+    String procesador;
+    
+    public void establecerColor (String c){
+        
+        color = c;
+    }
+    public String obtenerColor(){
+        
+        return color;
+    }
+    
+    public void establecerProcesador (String p){
+        
+        color = p;
+    }
+    public String ObtenerProcesador(){
+        
+        return procesador;
+    }
+    
+}
